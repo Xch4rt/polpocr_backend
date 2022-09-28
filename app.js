f9009a4b4dc0eeed3b7fb315d46c41d90c605213
Xch4rt <pab203.guti@gmail.com>
2022-09-27 22:22:50 -0600

    add: express.json()

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const user = require("./routes/user")
-
+const todo = require("./routes/todo")
 const app = express();
 
 
@@ -8,6 +8,10 @@ app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
 
+app.use(express.json())
+
 app.use('/api/v1', user)
+app.use('/api/v1', todo)
+
 
 
